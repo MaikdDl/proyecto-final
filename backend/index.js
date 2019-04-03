643e6e72f3933eef399e8362d45cd0c859c3ebe1
MaikdDl <maikdl@gmail.com>
2019-04-03 22:14:27 +0200

    Creados: servidor, conexión con mysql

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+require("dotenv").config();
+const webServer = require("./webserver");
+const httpServerConfig = require("./config/http-server-config");
+const mysqlPool = require('./databases/mysql-pool');
+
+(async function initApp() {
+  try {
+    await mysqlPool.connect();
+    await webServer.listen(httpServerConfig.port);
+    console.log(`Servidor iniciado no porto: ${httpServerConfig.port}`);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
