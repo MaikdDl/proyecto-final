a630a0a8f2e08f1b773953ffca5eaa330fb43a6a
MaikdDl <maikdl@gmail.com>
2019-04-09 21:01:57 +0200

    Corregido: layout

--- frontend/proyecto-mo/src/app/sobre-nos/sobre-nos-routing.module.ts
+++ frontend/proyecto-mo/src/app/sobre-nos/sobre-nos-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SobreNosComponent } from './component/sobre-nos/sobre-nos.component';
+import { LayoutComponent } from '../shared/layout/component/layout/layout.component';
 
 const routes: Routes = [
   {
-    path: 'sobre-nos',
-    component: SobreNosComponent
+    path: '',
+    redirectTo: 'sobre-nos',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'sobre-nos',
+        component: SobreNosComponent
+      }
+    ]
   }
 ];
 
