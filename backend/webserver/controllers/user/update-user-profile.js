8cd4a77563d9e7dba4e3ba022fd85088589ec948
MaikdDl <maikdl@gmail.com>
2019-05-01 19:00:21 +0200

    Solucionado: problema con la consulta a bbdd

--- backend/webserver/controllers/user/update-user-profile.js
+++ backend/webserver/controllers/user/update-user-profile.js
@@ -6,15 +6,15 @@ const Joi = require('joi');
 async function validate(payload) {
 
   const schema = {
-    nombre: Joi.string().min(3).max(64).required(),
-    apellido1: Joi.string().min(3).max(64).required(),
-    apellido2: Joi.string().min(3).max(64),
+    name: Joi.string().min(3).max(64).required(),
+    firstSurname: Joi.string().min(3).max(64).required(),
+    secondSurname: Joi.string().min(3).max(64),
     nif: Joi.string().min(5).max(15).required(),
-    direccion: Joi.string().min(15).max(255).required(),
-    cp: Joi.string().min(3).max(15).required(),
-    pais: Joi.string().min(3).max(45).required(),
-    telefono: Joi.number().integer().min(100000).max(99999999999).required(),
-    fecha_nacimiento: Joi.date().required(),
+    address: Joi.string().min(15).max(255).required(),
+    zipCode: Joi.string().min(3).max(15).required(),
+    country: Joi.string().min(3).max(45).required(),
+    phoneNumber: Joi.number().integer().min(100000).max(99999999999).required(),
+    birthday: Joi.date().required(),
   };
 
   return Joi.validate(payload, schema);
@@ -25,6 +25,8 @@ async function updateUserProfile(req, res, next) {
   const userDataProfile = { ...req.body };
   const { uuid } = req.claims;
   const now = new Date();
+  // let birthdayDate = new Date();
+
   const lastChange = now.toISOString().substring(0, 19).replace('T', ' ');
 
   try {
@@ -34,24 +36,29 @@ async function updateUserProfile(req, res, next) {
   }
 
   const {
-    nombre,
-    apellido1,
-    apellido2,
+    name,
+    firstSurname,
+    secondSurname,
     nif,
-    direccion,
-    cp,
-    pais,
-    telefono,
-    fecha_nacimiento,
+    address,
+    zipCode,
+    country,
+    phoneNumber,
+    birthday
   } = userDataProfile
+  // birthdayDate = birthday;
+  // birthdayOk = birthdayDate.toISOString().substring(0, 19).replace('T', ' ');
+  // const birthdayOk = birthday.toMysqlFormat();
+  const birthdayOk = Date.parse(birthday);
+
 
   try {
     const connection = await mysqlPool.getConnection();
 
     await connection.query(`UPDATE Proyecto_MO.cliente 
-    SET nombre = '${nombre}', apellido1 = '${apellido1}', apellido2 = '${apellido2}', 
-    nif = '${nif}', direccion = '${direccion}', cp = ${cp}, pais = '${pais}', telefono = ${telefono}, 
-    fecha_nacimiento = '${fecha_nacimiento}', ultimo_cambio = '${lastChange}'
+    SET nombre = '${name}', apellido1 = '${firstSurname}', apellido2 = '${secondSurname}', 
+    nif = '${nif}', direccion = '${address}', cp = ${zipCode}, pais = '${country}', telefono = ${phoneNumber}, 
+    fecha_nacimiento = FROM_UNIXTIME('${birthdayOk}'), ultimo_cambio = '${lastChange}'
     WHERE cliente_uuid = '${uuid}';`);
 
     return res.status(204).send();
